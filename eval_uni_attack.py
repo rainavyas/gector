c54f91430b7f054f8041eef504e01acdc2eb93fe
Vyas Raina <sayvaniar@gmail.com>
2022-06-08 21:17:37 +0100

    temp

--- eval_uni_attack.py
+++ eval_uni_attack.py
@@ -16,11 +16,12 @@ def count_edits(input_file, model, attack_phrase=''):
     test_data = read_lines(input_file)
     cnt_corrections = 0
     num_0_edits = 0
-    for i,sent in enumerate(test_data[:5]):
+    for i,sent in enumerate(test_data[:10]):
         print(f'On {i}/{len(test_data)}')
         sent_attack = sent + ' ' + attack_phrase + ' .'
         pred, cnt = model.handle_batch([sent_attack.split()])
-        print(sent_attack, pred)
+        if cnt !=0:
+            print(sent_attack, pred)
         cnt_corrections += cnt
         if cnt == 0:
             num_0_edits +=1
